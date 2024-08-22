b7f7b90e87196579bfee7e530d132e3695055173
aniebietafia <aniebietafia87@gmail.com>
2024-08-22 11:09:00 +0100

    creating database command

---
+++ postgres-commands/01-create-database.sql
@@ -0,0 +1,6 @@
+-- create database
+CREATE DATABASE talent_hive;
+-- if multiple words, use _ to separate each word
+-- CREATE DATABASE IF NOT EXISTS talent_pool;
+-- database name can also be wrapped in quotes
+-- CREATE DATABASE IF NOT EXISTS "world_talents";
