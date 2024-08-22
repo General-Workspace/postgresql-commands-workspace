4b15ac522633dccc1e80cf2e7473b680abba54ec
aniebietafia <aniebietafia87@gmail.com>
2024-08-22 12:53:26 +0100

    inserting data into table

--- postgres-commands/01-create-database.sql
+++ postgres-commands/01-create-database.sql
@@ -1,6 +1,6 @@
 -- create database
 CREATE DATABASE talent_hive;
--- if multiple words, use _ to separate each word
+-- other ways of creating database
 -- CREATE DATABASE IF NOT EXISTS talent_pool;
 -- database name can also be wrapped in quotes
 -- CREATE DATABASE IF NOT EXISTS "world_talents";
---
+++ postgres-commands/03-insert-users-data.sql
@@ -0,0 +1,13 @@
+-- inserting data into table
+INSERT INTO users (
+        full_name,
+        yearly_salary,
+        current_status
+    )
+VALUES (
+        'Aniebiet Afia',
+        80000,
+        'employed'
+    );
+INSERT INTO users (full_name, yearly_salary, current_status)
+VALUES ('Michael Whyte', 0, 'unemployed');
