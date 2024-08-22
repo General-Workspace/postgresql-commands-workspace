ff8b9ec962eb782199f4a7f234f0209bd0d51984
aniebietafia <aniebietafia87@gmail.com>
2024-08-22 13:43:36 +0100

    inserting into employers table

---
+++ postgres-commands/07-insert-data.sql
@@ -0,0 +1,43 @@
+-- INSERT INTO employers (
+--         company_name,
+--         company_address,
+--         yearly_revenue,
+--         is_hiring
+--     )
+-- VALUES (
+--         'Brints Group',
+--         '580 Brints Group Avenue, High View City, Oakland',
+--         128307122.271,
+--         true
+--     ) 
+-- INSERT INTO employers (
+--         company_name,
+--         company_address,
+--         yearly_revenue,
+--         is_hiring
+--     )
+-- VALUES (
+--         'Bellamy Real',
+--         '123 Bentamy Close, Serba Donk, Blue Ocean',
+--         7309270.738,
+--         false
+--     )
+-- insert many data
+INSERT INTO employers (
+        company_name,
+        company_address,
+        yearly_revenue,
+        is_hiring
+    )
+VALUES (
+        'NNPC Limited',
+        'Betany Estate, FCT, Abuja',
+        112673.675,
+        TRUE
+    ),
+    (
+        'ECOWAS, Nigeria',
+        '34 Maryland Jest, FCT, Abuja',
+        45290.83,
+        FALSE
+    );
